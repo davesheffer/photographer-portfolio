b551182a475d8bb17f656deb6fff38439d8c9431
David Sheffer <dave.sheffer1@gmail.com>
2025-02-01 21:01:12 +0200

    done

--- src/components/galleryMulti/GalleryMultiSlide.jsx
+++ src/components/galleryMulti/GalleryMultiSlide.jsx
@@ -5,8 +5,7 @@ import Image from "next/image";
 const  GalleryMultiSilde = ({ image }) => {
  
     return (
-      <div className="flex text-white item-center justify-center h-[550px] overflow-hidden">
-     
+      <div className="flex text-white item-center justify-center  h-auto md:h-[550px] overflow-hidden">
           <Image src={image.src} alt={image.alt} quality={100} style={{objectFit: "cover"}} className="w-full h-auto p-4" />  
       </div>
     );
